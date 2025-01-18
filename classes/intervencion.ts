6685ade64fdd6b11c96b87227de8088b794c3ce7
Emilio Rodríguez <emiliorb26@gmail.com>
2025-01-18 00:00:13 -0600

    fix: :bug: Corregir la obtención de conexión y el manejo de resultados en el método getIntervencion de la clase Inventario

--- classes/intervencion.ts
+++ classes/intervencion.ts
@@ -1,27 +1,29 @@
-import pool from '../environment/environment';
-import { PoolClient } from 'pg';
+import pool from "../environment/environment";
 
 export class Inventario {
-
     public pool = pool;
 
     public constructor() {}
 
     public async getIntervencion(id: any) {
-        const connection: PoolClient = await this.pool.connect();
+        const connection = await this.pool.getConnection();
         try {
-            const result = await connection.query('CALL sp_tenant_rw_intervencion($1)', [(id == 0) ? null : id]);
-            const rows = result.rows;
-            if (rows.length > 0) {
-                return rows;
+            const [rows]: any[] = await connection.query(
+                "CALL sp_tenant_rw_intervencion(?)",
+                [id == 0 ? null : id]
+            );
+            if (rows[0].length > 0) {
+                return rows[0];
             } else {
                 return {
                     response: "¡El usuario no existe!",
-                    tipo: false
-                }
+                    tipo: false,
+                };
             }
         } catch (err: any) {
-            throw new Error(`Se presento un error en el procedimiento ${err.procName}...${err.message}`);
+            throw new Error(
+                `Se presento un error en el procedimiento ${err.procName}...${err.message}`
+            );
         } finally {
             connection.release();
         }
